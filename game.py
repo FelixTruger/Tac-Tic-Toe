1c0b174b0c8f499770d65a064df94c60a7f6546c
it14078 <it14078@lehre.dhbw-stuttgart.de>
2016-05-16 21:38:22 +0200

    added basic game as module, renamed ttt.py to main.py

---
+++ game.py
@@ -0,0 +1,124 @@
+from array import *
+import random
+
+field = [[]]
+player = 'X' # x begins the game
+Size = 0
+bot = 0
+
+def setMove(r, c):
+  global player
+  
+  # cant move when already set
+  if getValue(r, c):
+    print(player+" tried an invalid move")
+    return 0
+  
+  # set the move to the field for player
+  field[r][c] = player;
+  
+  print(player+" moved on ["+str(r)+"] ["+str(c)+"]")
+  
+  # switch player
+  if player is 'X':
+    player = 'O'
+  else:
+    player = 'X'
+  
+  if bot and player is 'O':
+    botMove()
+  else:
+    print(player+" has the next move")
+  
+  return 1
+
+def getValue(r, c):
+  #print("Value read from ["+str(r)+"] ["+str(c)+"]")
+  return field[r][c]
+
+def botMove():
+  row = random.randint(0, Size-1)
+  col = random.randint(0, Size-1)
+  
+  # retry if move was not valid
+  if not setMove(row, col):
+    botMove()
+    
+
+def finished():
+  global Size
+  
+   # search for rows all the same value
+  for row in range(Size):
+    current = 'start'
+    for col in range(Size):
+      val = getValue(row, col)
+      if current is 'start' or current is val and val is not '':
+        current = getValue(row, col)
+        if col is Size-1:
+          return current
+      else:
+        current = 'no win' 
+        
+  # search for cols all the same value
+  for col in range(Size):
+    current = 'start'
+    for row in range(Size):
+      val = getValue(row, col)
+      if current is 'start' or current is val and val is not '':
+        current = getValue(row, col)
+        if row is Size-1:
+          return current
+      else:
+        current = 'no win' 
+  
+  # search first diagonal for the same value
+  current = 'start'
+  for cr in range(Size):
+    val = getValue(cr, cr)
+    if current is 'start' or current is val and val is not '':
+      current = getValue(cr, cr)
+      if cr is Size-1:
+        return current
+    else:
+      current = 'no win'     
+      
+  # search second diagonal for the same value
+  current = 'start'
+  for cr in range(Size):
+    val = getValue(cr, Size-1-cr)
+    if current is 'start' or current is val and val is not '':
+      current = getValue(cr, Size-1-cr)
+      if cr is Size-1:
+        return current
+    else:
+      current = 'no win'   
+  
+  # last but not least: check whether all fields have been set
+  for col in range(Size):
+    current = 'start'
+    for row in range(Size):
+      if getValue(row, col) is '':
+        return 0  
+  
+  return "Keiner der beiden Spieler"
+
+def startGame(Gamesize, usebot):
+  global bot
+  global Size
+  global player
+  
+  bot = int(usebot)
+  Size = int(Gamesize)
+  
+  print("Game started with size "+str(Size));
+  global field 
+  field = [['' for x in range(Size)] for y in range(Size)] 
+  #print("Field initialized")
+  player = 'X';
+  print(player+" starts the game")
+  
+  if bot:
+    print("Opponent is bot")
+  else:
+    print("Opponent is human")
---
+++ main.py
@@ -0,0 +1,111 @@
+import random
+
+from functools import partial
+# partial enables us to make a function wrapper for buttons
+
+import tkinter as tk
+import tkinter.messagebox as tkMsg
+import socket
+import game
+
+MainWindow = tk.Tk()
+Buttonsize = 5;
+
+def showMap(Gamesize, bot):
+    Size = int(Gamesize)
+    game.startGame(Gamesize, int(bot))
+    buttons = [[None for x in range(Size)] for y in range(Size)]
+    
+    # in case the window was used before, clean it up
+    clearWindow(MainWindow)
+    
+    # add Gamesize*Gamesize buttons in 2 dimensions to MainWindow
+    for r in range(Size):
+        for c in range(Size):
+          state = str(game.getValue(r, c))
+          newButton = tk.Button(MainWindow, text=state, borderwidth=1)
+          newButton.config(height=Buttonsize, width=Buttonsize);
+          newButton.grid(row=r, column=c)
+          newButton["command"] = partial(Move, r, c, newButton)
+          buttons[r][c] = newButton
+          
+    # add Menu button to MainWindow
+    def localShowMenu():
+        if tkMsg.askyesno("Spiel beenden?", "Möchten Sie das Spiel beenden und in das Menü zurückkehren?"):
+            showMenu()
+            
+    btn_Menu = tk.Button(MainWindow, text='Menü')
+    btn_Menu["command"] = localShowMenu
+    btn_Menu.config(width=Size*Buttonsize)
+    btn_Menu.grid(row=Size, column=0, columnspan=Size) 
+
+def Move(r, c, btn):
+    result = game.setMove(r, c)
+    if result is 1:
+        btn["text"] = game.getValue(r, c)
+    
+    winner = game.finished()
+    if winner:
+        tkMsg.showinfo("Spielende", str(winner)+" hat das Spiel gewonnen!")
+        showMenu()
+
+def closeMenu():
+    MainWindow.destroy()
+
+def showMenu():
+    def localStartGame(bot):
+        showMap(txt_gamesize.get(), int(bot))
+        
+    # in case the window was used before, clean it up
+    clearWindow(MainWindow)
+
+    lbl_gamesize = tk.Label(MainWindow)
+    txt_gamesize = tk.Entry(MainWindow)
+    btn_KI = tk.Button(MainWindow)
+    lbl_player = tk.Label(MainWindow)
+    btn_newGame = tk.Button(MainWindow)
+    btn_join = tk.Button(MainWindow)
+    lbl_IP = tk.Label(MainWindow)
+    txt_IP = tk.Entry(MainWindow)
+    btn_exit = tk.Button(MainWindow)
+    lbl_KI = tk.Label(MainWindow)
+
+    lbl_gamesize["text"] = "Spielfeldgröße"
+    btn_KI["text"] = "Neues Spiel starten"
+    lbl_player["text"] = "Gegen anderen Spieler"
+    btn_newGame["text"] = "Neues Spiel starten"
+    btn_join["text"] = "an Spiel teilnehmen"
+    lbl_IP["text"] = "IP Adresse"
+    btn_exit["text"] = "Beenden"
+    txt_gamesize.insert(0, "3")
+    lbl_KI["text"] = "Gegen KI"
+
+    btn_exit["command"] = closeMenu
+    btn_KI["command"] = partial(localStartGame, 0) # 1 means to use bot as opponent
+
+    lbl_gamesize.grid(row=0, column=0)
+    txt_gamesize.grid(row=0, column=1)
+    lbl_KI.grid(row=1, column=0)
+    btn_KI.grid(row=1, column=1)
+    lbl_player.grid(row=2, column=0)
+    btn_newGame.grid(row=2, column=1)
+    btn_join.grid(row=3, column=1)
+    lbl_IP.grid(row=4, column=0)
+    txt_IP.grid(row=4, column=1)
+    btn_exit.grid(row=5, column=1)
+
+    MainWindow.mainloop()
+
+def clearWindow(tkWindow):
+	# remove everything from MainWindow
+    for widget in tkWindow.winfo_children():
+      widget.destroy()
+
+if __name__ == '__main__':
+    print("Tac-Tic-Toe  Copyright (C) 2016  Felix & Hagen\n\
+    This program comes with ABSOLUTELY NO WARRANTY; for details see GPL.\n\
+    This is free software, and you are welcome to redistribute it\n\
+    under certain conditions; see GPL for details.")
+    showMenu()
+
+
